a06f355ef622ab6ddc33303d09ff5e8baeb5f746
Nowrinsultana <c213250@ugrad.iiuc.ac.bd>
2025-09-13 23:39:17 +0600

    pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+'use client'
+
 export default function Home() {
   return (
     <div>
       {' '}
-      <p> You are not signed in </p> <button> Sign in with Github </button>{' '}
+      <p> You are not signed in </p>{' '}
+      <button onClick={() => SignIn('Github')}> Sign in with Github </button>{' '}
     </div>
   )
 }
---
+++ lib/actions/auth.ts
@@ -0,0 +1,7 @@
+'use server'
+
+import { signIn, signOut } from 'next-auth/react'
+
+export const login = async () => {}
+
+export const logout = async () => {}
